75e966debace55d5f6a178b702e0a1abc3a5ca86
jono <jono.stretch@gmail.com>
2017-12-15 14:53:59 +0200

    breaks on codesandbox

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './App';
 
 const Root = ({ store }) => {
   return (
     <Provider store={store}>
-      <App />
+      <Router history={browserHistory}>
+        <Route path="/" component={App} />
+      </Router>
     </Provider>
   );
+};
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired
 };
 
 export default Root;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Root from './components/Root';
-import configureStore from './configureStore';
+
+import configureStore from './utils/configureStore';
 const store = configureStore();
+
 ReactDOM.render(<Root store={store} />, document.getElementById('root'));
